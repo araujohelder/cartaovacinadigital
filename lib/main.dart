6baa4f3adab2a1d85319f991d18f3928906c8869
Helder <you@example.com>
2019-05-18 11:04:29 -0300

    Alterando titulo do APP

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Carteira de vacinação digital',
+      title: 'Prontuário único digital',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
